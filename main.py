6888e5eb1db47b3bb9bba7b11f176b529fc5d7bf
Doctorspeppers <doctorspeppers@gmail.com>
2022-04-04 14:12:50 -0300

    Agora vai

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from .packages.flask import Flask
 from .packages.flask import request
 app = Flask(__name__)
 
-@app.route("/latest/meta-data/tags/instance/<env>")
-def hello_world(env):
-    with open("./envs/"+env,"r") as fp:
-
-        return str(fp.contents)
+@app.route("/latest/meta-data/tags/instance/<var>")
+def hello_world(var):
+    env_vars = []
+    with open("./.env","r") as fp:
+        lines = fp.readlines()
+        for line in lines:
+            env_vars[line.split("=")[0]] = line.split("=")[1]
+        return env_vars[var]
