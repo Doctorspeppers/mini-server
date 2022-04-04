563731d5afb22466732628cd73a50b70c9b2bf7b
Doctorspeppers <doctorspeppers@gmail.com>
2022-04-04 15:08:47 -0300

    add port

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask import request
 app = Flask(__name__)
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
 @app.route("/latest/meta-data/tags/instance/<var>")
 def hello_world(var):
     env_vars = []
