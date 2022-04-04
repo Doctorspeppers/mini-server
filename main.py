f370a34b43e148d9bb4fa704521a0a9b629b4b35
Doctorspeppers <doctorspeppers@gmail.com>
2022-04-04 14:03:06 -0300

    Change url route

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from .packages.flask import Flask
 from .packages.flask import request
 app = Flask(__name__)
 
-@app.route("/<env>")
+@app.route("/latest/meta-data/tags/instance/<env>")
 def hello_world(env):
     with open("./envs/"+env,"r") as fp:
 
